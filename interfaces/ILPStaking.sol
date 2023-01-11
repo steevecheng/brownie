d6e6bfdf9b531e7c0601eb8d11a48f4f9dff9034
steevecheng <steevecheng.coin@gmail.com>
2023-01-11 15:28:37 +0800

    add ILPStaking.sol

---
+++ interfaces/ILPStaking.sol
@@ -0,0 +1,30 @@
+pragma solidity ^0.6.0;
+
+// SPDX-License-Identifier: MIT
+
+interface LPStaking {
+
+    function poolLength() external view returns (uint256);
+
+    function set(uint256 _pid, uint256 _allocPoint) external;
+
+    function getMultiplier(uint256 _from, uint256 _to) external;
+
+    function pendingStargate(uint256 _pid, address _user) external view returns (uint256);
+
+    function massUpdatePools() external;
+
+    function updatePool(uint256 _pid) external;
+
+    function deposit(uint256 _pid, uint256 _amount) external;
+
+    function withdraw(uint256 _pid, uint256 _amount) external;
+    
+    function emergencyWithdraw(uint256 _pid) external;
+
+    function safeStargateTransfer(address _to, uint256 _amount) external;
+
+    function setStargatePerBlock(uint256 _stargatePerBlock) external;
+
+    function renounceOwnership() external;
+}
